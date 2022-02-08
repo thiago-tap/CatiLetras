d177e527aa55b3c61a4575f4966be49b3e62354f
Thiago de Paula <thiago@tenil.com.br>
2022-02-08 16:21:33 -0300

    Tradução

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,19 +1,18 @@
-export const GAME_TITLE = 'Reactle'
+export const GAME_TITLE = 'CatiLetras!'
 
-export const WIN_MESSAGES = ['Great Job!', 'Awesome', 'Well done!']
-export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
-export const ABOUT_GAME_MESSAGE = 'About this game'
-export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
-export const WORD_NOT_FOUND_MESSAGE = 'Word not found'
-export const CORRECT_WORD_MESSAGE = (solution: string) =>
-  `The word was ${solution}`
+export const WIN_MESSAGES = ['Ótimo trabalho!', 'Incrível', 'Muito bem!']
+export const GAME_COPIED_MESSAGE = 'Jogo copiado para a área de transferência'
+export const ABOUT_GAME_MESSAGE = 'Sobre este jogo'
+export const NOT_ENOUGH_LETTERS_MESSAGE = 'Não há letras suficientes'
+export const WORD_NOT_FOUND_MESSAGE = 'Palavra não encontrada'
+export const CORRECT_WORD_MESSAGE = (solution: string) => `A palavra era ${solution}`
 export const ENTER_TEXT = 'Enter'
-export const DELETE_TEXT = 'Delete'
-export const STATISTICS_TITLE = 'Statistics'
-export const GUESS_DISTRIBUTION_TEXT = 'Guess Distribution'
-export const NEW_WORD_TEXT = 'New word in'
-export const SHARE_TEXT = 'Share'
-export const TOTAL_TRIES_TEXT = 'Total tries'
-export const SUCCESS_RATE_TEXT = 'Success rate'
-export const CURRENT_STREAK_TEXT = 'Current streak'
-export const BEST_STREAK_TEXT = 'Best streak'
+export const DELETE_TEXT = 'Apagar'
+export const STATISTICS_TITLE = 'Estatisticas'
+export const GUESS_DISTRIBUTION_TEXT = 'Distribuição de palpites'
+export const NEW_WORD_TEXT = 'Nova palavra em'
+export const SHARE_TEXT = 'Compartilhar'
+export const TOTAL_TRIES_TEXT = 'Total de tentativas'
+export const SUCCESS_RATE_TEXT = 'Taxa de sucesso'
+export const CURRENT_STREAK_TEXT = 'Sequência atual'
+export const BEST_STREAK_TEXT = 'Melhor sequência'
