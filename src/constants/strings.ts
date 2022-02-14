c9f8e7b3f3b0bde2a8d42b072ac10c4f80dc5370
Thiago de Paula <thiago@tenil.com.br>
2022-02-14 11:10:49 -0300

    Atualizando o nome do jogo!

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,4 +1,4 @@
-export const GAME_TITLE = process.env.REACT_APP_GAME_NAME!
+export const GAME_TITLE = 'CatiLetras!'
 
 export const WIN_MESSAGES = ['Ótimo trabalho!', 'Incrível', 'Muito bem!']
 export const GAME_COPIED_MESSAGE = 'Jogo copiado para a área de transferência'
