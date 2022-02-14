530e9a4e1ea26ce6528a9e4ce3594b346355f768
Thiago de Paula <thiago@tenil.com.br>
2022-02-14 10:48:43 -0300

    Ajustes no jogo! Formatacao do codigo

--- src/components/grid/Grid.tsx
+++ src/components/grid/Grid.tsx
@@ -2,31 +2,24 @@ import { MAX_CHALLENGES } from '../../constants/settings'
 import { CompletedRow } from './CompletedRow'
 import { CurrentRow } from './CurrentRow'
 import { EmptyRow } from './EmptyRow'
-import { removeDiacritics } from '../../lib/words'
-import { WORDS } from '../../constants/wordlist'
 
 type Props = {
   guesses: string[]
   currentGuess: string
   isRevealing?: boolean
+  currentRowClassName: string
 }
 
-export const Grid = ({ guesses, currentGuess, isRevealing }: Props) => {
+export const Grid = ({
+  guesses,
+  currentGuess,
+  isRevealing,
+  currentRowClassName,
+}: Props) => {
   const empties =
     guesses.length < MAX_CHALLENGES - 1
       ? Array.from(Array(MAX_CHALLENGES - 1 - guesses.length))
       : []
-
-  const arrPalavra = []
-
-  for (let i = 0; i < guesses.length; i++) {
-    const palavra = WORDS.find(
-      // eslint-disable-next-line no-loop-func
-      (word) => removeDiacritics(word) === removeDiacritics(guesses[i])
-    )
-    arrPalavra.push(String(palavra).toUpperCase())
-  }
-  guesses = arrPalavra
 
   return (
     <div className="pb-6">
@@ -37,8 +30,9 @@ export const Grid = ({ guesses, currentGuess, isRevealing }: Props) => {
           isRevealing={isRevealing && guesses.length - 1 === i}
         />
       ))}
-      {guesses.length < MAX_CHALLENGES && <CurrentRow guess={currentGuess} />}
-
+      {guesses.length < MAX_CHALLENGES && (
+        <CurrentRow guess={currentGuess} className={currentRowClassName} />
+      )}
       {empties.map((_, i) => (
         <EmptyRow key={i} />
       ))}
