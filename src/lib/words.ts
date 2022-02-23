a615bd7c391ba4d5d0b312f94c097ac024a25e5a
Thiago de Paula <thiago@tenil.com.br>
2022-02-23 17:15:06 -0300

    ajuste formatação

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -45,10 +45,10 @@ export const getWordOfDay = () => {
   const epochMs = new Date('January 1, 2022 00:00:00').valueOf()
   const now = Date.now()
   const msInDay = 86400000
-  //const index = Math.floor((now - epochMs) / msInDay)
-  const index =
+  const index = Math.floor((now - epochMs) / msInDay)
+  /*const index =
     Math.floor(Math.random() * (Math.floor(13200) - Math.ceil(0) + 1)) +
-    Math.ceil(0)
+    Math.ceil(0)*/
   const nextday = (index + 1) * msInDay + epochMs
 
   return {
