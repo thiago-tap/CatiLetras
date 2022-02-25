7bb35f742afbea80df53cf4bdaef14522e799dcd
Thiago de Paula <thiago@tenil.com.br>
2022-02-25 16:07:55 -0300

    Tradução da modal de Informação

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -10,24 +10,24 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Guess the word in 6 tries. After each guess, the color of the tiles will
-        change to show how close your guess was to the word.
+        Adivinhe a palavra em 6 tentativas. Após cada palpite, a cor das peças
+        será mude para mostrar o quão próximo seu palpite estava da palavra.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
         <Cell
           isRevealing={true}
           isCompleted={true}
-          value="W"
+          value="L"
           status="correct"
         />
         <Cell value="E" />
-        <Cell value="A" />
+        <Cell value="T" />
         <Cell value="R" />
-        <Cell value="Y" />
+        <Cell value="A" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter W is in the word and in the correct spot.
+        A letra L está na palavra e no local correto.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
@@ -36,35 +36,35 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         <Cell
           isRevealing={true}
           isCompleted={true}
-          value="L"
+          value="S"
           status="present"
         />
-        <Cell value="O" />
         <Cell value="T" />
+        <Cell value="A" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter L is in the word but in the wrong spot.
+        A letra S está na palavra, mas no lugar errado.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="V" />
-        <Cell value="A" />
-        <Cell value="G" />
-        <Cell isRevealing={true} isCompleted={true} value="U" status="absent" />
+        <Cell value="M" />
         <Cell value="E" />
+        <Cell value="T" />
+        <Cell isRevealing={true} isCompleted={true} value="R" status="absent" />
+        <Cell value="O" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter U is not in the word in any spot.
+        A letra U não está na palavra em nenhum lugar.
       </p>
 
       <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
-        This is an open source version of the word guessing game we all know and
-        love -{' '}
+        Esta é uma versão de código aberto do jogo de adivinhação de palavras
+        que todos conhecemos e amamos -{' '}
         <a
           href="https://github.com/cwackerfuss/react-wordle"
           className="underline font-bold"
         >
-          check out the code here
+          veja o código aqui!
         </a>{' '}
       </p>
     </BaseModal>
